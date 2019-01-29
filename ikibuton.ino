224b7c657278162dc368d272cd18fb6822e3f3f3
sprcode <47007407+sprcode@users.noreply.github.com>
2019-01-29 15:06:54 +0300

    Add files via upload

---
+++ ikibuton.ino
@@ -0,0 +1,15 @@
+int led=10;
+int buton1=5;
+int buton2=6;
+void setup() {
+pinMode(led,OUTPUT);
+pinMode(buton1,INPUT);
+pinMode(buton2,INPUT);
+}
+void loop() {
+if(digitalRead(buton_1)== 1){
+digitalWrite(led,HIGH);
+}
+if(digitalRead(buton_2)== 1){
+digitalWrite(led,LOW);
+}
